504a470a7fd6cb7af9d2ca74afc5a117bffabd95
nhc17 <hlncg2@gmail.com>
2018-10-23 21:21:17 +0800

    GET authors and single author

--- server/main.js
+++ server/main.js
@@ -1,9 +1,106 @@
 const express = require('express'),
       path = require('path'),
+      admin = require('firebase-admin'),
+      googleStorage = require('@google-cloud/storage'),
       bodyParser = require('body-parser'),
       cors = require('cors');
 
 const app = express();
+
+const API_URI = "/api";
+
+// Initialize Firebase
+const credFile = process.env.Svc_Cred_File || "./f.json";
+
+var serviceAccount = require(credFile);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://mini-bbb5b.firebaseio.com"
+});
+
+var db = admin.firestore();
+
+var authorsCollection = db.collection('authors');
+var topicsCollection = db.collection('topics');
+var articlesCollection = db.collection('articles');
+
+//export Google_Application_Credentials
+/*const gStorage = googleStorage({
+      projectId: "mini-bbb5b"
+});
+
+const bucket = gStorage.bucket("mini-bbb5b.appspot.com");
+const googleMulter = multer({
+    storage: multer.memoryStorage(),
+    limits: {
+        fileSize: 20 * 1024 * 1024 //20MB
+    }
+})
+*/
+/////////////////////////// READ ///////////////////////////////////////////
+// GET array of authors
+app.get(API_URI + '/authors/search', (req, res) => {
+    authorsCollection
+    .get()
+    // console.log(authorsCollection)
+    .then(snapshot => {
+        let authorsArr = [];
+        snapshot.forEach(doc => {
+            console.log(doc.id, '=>', doc.data());
+            authorsArr.push(doc.data());       
+    });
+    res.status(200).json(authorsArr);
+   })
+   .catch(err => {
+     console.log('Error getting documents', err);
+  }); 
+});
+
+// GET one author with associated title
+app.get(API_URI + '/author/search', (req, res) => {
+    let firstname = req.query.firstname;
+    let lastname = req.query.lastname;
+  //let titles = req.query.title
+    console.log(firstname, lastname);
+    if (typeof(firstname === 'undefined') 
+        && typeof(lastname === 'undefined')) {
+        //&& typeof(titles === "undefined")){
+        if (firstname === ''
+        && lastname === ''
+      //  && titles === ''){
+         ) {
+                console.log('firstname, lastname, topic and article is undefined');
+            res.status(500).json({error: "firstname, lastname, topic & article are undefined"});
+        }
+    }
+    authorsCollection
+        .where('firstname', '==', firstname)
+        .where('lastname', '==', lastname)
+        .limit(10)
+    //articlesCollection
+       // .where('title', '==', titles)
+    .get()
+    .then(snapshot => {
+        console.log(">>>snapshot");
+        let authorsArr = [];
+        let articlesArr = [];
+        snapshot.forEach(doc => {
+            console.log(doc.id, '=>', doc.data());
+            authorsArr.push(doc.data());
+            //articlesArr.push(doc.data());
+        });
+        res.status(200).json(authorsArr, /*articlesArr*/);
+      })
+      .catch(err => {
+          console.log('Error getting documents', err);
+          res.status(500).json(err);
+     });
+  });
+
+
+
+
 
 const PORT = parseInt(process.argv[2]) || parseInt(process.env.APP_PORT) || 3000;
 app.listen(PORT, () => {
